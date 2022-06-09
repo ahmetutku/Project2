6a1531a6409129fbaa9438f67d4b1e2faa714540
ahmetutku <ahamamcioglu2014@gmail.com>
2022-06-09 10:45:00 +0300

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Ahmet Hamamcioglu on 9.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
